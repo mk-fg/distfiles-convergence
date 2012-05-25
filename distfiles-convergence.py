fd0885680d87a26d4291b529da8f057a8a7e66d2
Mike Kazantsev <mk.fraggod@gmail.com>
2012-05-25 18:27:43 +0600

    A bit more complete implementation

--- distfiles-convergence.py
+++ distfiles-convergence.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import itertools as it, operator as op, functools as ft
+from time import time
 import os, sys, xmlrpclib, hashlib, json
 
 
@@ -25,18 +26,35 @@ class ManifestShelveDB(object):
 		if vv == '\1': return v
 		raise NotImplementedError('Unknown value encoding version: {}'.format(ord(vv)))
 
-	def __setitem__(self, k, v):
-		self.db[k] = '\1' + json.encode(v)
+	def __setitem__(self, k, meta):
+		self.db[k] = '\1' + json.encode(meta)
 
 	def __del__(self): self.db.close()
 
+	def undergoal_order(self, qmin, qmax, limit, mirrors):
+		queue = list()
+		for path, meta in self.db.viewitems():
+			meta_mirrors = meta.get('mirrors', dict())
+			prio = dict(
+				(mtype, len(set(
+					meta_mirrors.get(mtype, set()) ).intersection(mirrors)))
+				for mtype in ['inconsistent', 'consistent', 'unavailable'] )
+			if prio['consistent'] >= qmax: continue # this one is fine
+			# prio = inconsistencies, checks to get qmin, checks to get qmax, known-availability
+			prio = prio['inconsistent'], max(0, qmin - prio['consistent']),\
+				max(0, qmax - prio['consistent']), -prio['unavailable']
+			queue.append((prio, path))
+			if len(queue) > limit: break
+		return set(it.imap(op.itemgetter(1), sorted(queue)))
 
 
 def check_fs( path, db,
 		hashes=['md5', 'sha1', 'sha512'],
-		ignore_mtime=False, bs=2 * 2**20 ):
+		ignore_mtime=False, bs=2 * 2**20,
+		ts=None ):
 	from os.path import join, realpath
 
+	if not ts: ts = time()
 	if not isinstance(hashes, dict):
 		hashes = dict((alg, getattr(hashlib, alg)) for alg in hashes)
 
@@ -45,32 +63,40 @@ def check_fs( path, db,
 			path = realpath(join(root, path))
 			path_mtime = os.stat(path).st_mtime
 
-			meta_update = False
 			try: meta = db[path]
-			except KeyError: meta, meta_update = dict(), True
+			except KeyError: meta = dict()
 
-			if meta and path_mtime == meta.get('mtime'): continue
-
-			src_hashes = dict( (alg, func())
-				for alg,func in hashes.viewitems() )
-			with open(path, 'rb') as src:
-				for chunk in iter(ft.partial(src.read, bs), ''):
-				for chksum in src_hashes.viewvalues(): chksum.update(chunk)
-			meta_hashes = meta.get('hashes', dict())
-			for alg,chksum in src_hashes.viewitems():
-				chksum = src_hashes[alg] = chksum.hexdigest()
-				if alg in meta_hashes:
-					if meta_hashes[alg] != chksum:
-						log.warn('Detected change in file contents: {}'.format(path))
-						meta_update = True
-				else: meta_update = True
-
-			if meta_update:
+			if meta and path_mtime != meta.get('mtime'):
+				src_hashes = dict( (alg, func())
+					for alg,func in hashes.viewitems() )
+				with open(path, 'rb') as src:
+					for chunk in iter(ft.partial(src.read, bs), ''):
+					for chksum in src_hashes.viewvalues(): chksum.update(chunk)
+				meta_hashes = meta.get('hashes', dict())
+				for alg,chksum in src_hashes.viewitems():
+					chksum = src_hashes[alg] = chksum.hexdigest()
+					if alg in meta_hashes:
+						if meta_hashes[alg] != chksum:
+							log.warn('Detected change in file contents: {}'.format(path))
+							meta_update = True
+					else: meta_update = True
 				meta.update(dict(mtime=path_mtime, hashes=src_hashes))
-				db[path] = meta
+
+			meta['ts'] = ts
+			db[path] = meta
 
 
-def check_remote(db, remote): pass # TODO
+def check_gc(db, timeout):
+	ts_min = time() - timeout
+	for k in list( path for path, meta
+		in db.viewitems() if meta['ts'] < ts_min ): del db[k]
+
+
+def check_remote(paths, db, mirrors):
+	for path in paths:
+		meta = db[path]
+		# TODO: from here
+
 
 
 def main():
@@ -99,8 +125,28 @@ def main():
 		logging.WARNING if not optz.debug else logging.DEBUG )
 
 	## Update the manifest
+	check_fs_ts = time()
 	manifest_db = dict(shelve=ManifestShelveDB)[cfg.manifest.type](cfg.manifest)
-	for path in cfg.local: check_fs(path, manifest_db, hashes=cfg.manifest.hashes)
+	for path in cfg.local:
+		check_fs(path, manifest_db, hashes=cfg.manifest.hashes, ts=check_fs_ts)
+	## Manifest cleanup
+	check_gc(manifest_db, cfg.goal.gc_timeout * 24 * 3600)
+
+	## Build a large-enough list of stuff to be checked
+	qratio = int(len(cfg.remote) * cfg.goal.query.ratio)
+	qmin = cfg.goal.query.hard_min or qratio
+	qmax = max(qmin, cfg.goal.query.hard_max or qratio)
+	undergoal = manifest_db.undergoal_order(
+		qmin, qmax, limit=cfg.load.limit.files, mirrors=cfg.remote )
+	# Check that each listed path actually exists now
+	for path in list(undergoal):
+		try:
+			meta = db[path]
+			if meta['ts'] != check_fs_ts and not os.path.exists(path):
+				raise KeyError(path)
+		except KeyError:
+			log.debug('Skipping check for unlisted/nx path: {}'.format(path))
+			undergoal.remove(path)
 
 	## Check the manifest against remotes
-	# TODO
+	check_remote(undergoal, manifest_db, cfg.remote)
